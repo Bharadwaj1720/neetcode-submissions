9c1b82c82bd4895de4a3168114403c3d216de84b
Bharadwaj Vuppala <89703924+Bharadwaj1720@users.noreply.github.com>
2026-05-05 14:46:41 -0400

    Add: gas-station - submission-2

---
+++ Data Structures & Algorithms/gas-station/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n=len(gas)
+        if sum(gas)<sum(cost):
+            return -1
+        result=[]
+        for i in range(n):
+            result.append(gas[i]-cost[i])
+        temp=0
+        i=0
+        r=0
+       
+        while i <n:
+            temp+=result[i]
+            if temp<0:
+                temp=0
+                r=i+1
+            i=i+1
+        return r
+
+                 
+
+        
