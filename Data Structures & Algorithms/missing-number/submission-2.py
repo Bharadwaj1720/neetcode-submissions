7ca49b664718cebcb49e3260e5af1ebe409716f9
Bharadwaj Vuppala <89703924+Bharadwaj1720@users.noreply.github.com>
2026-05-07 23:23:56 -0400

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n=len(nums)
+        x=0
+        y=nums[0]
+        for i in range(1,n):
+            x^=i
+            y^=nums[i]
+            
+        return x^n^y
+
+        
